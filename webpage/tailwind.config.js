2b7d3c7012e18e547f557223567ada6eacf59653
Nigatu Paulos <nigatujeno@gmail.com>
2025-12-03 00:13:15 -0500

    Add markdown read and edit feature for knowledge base

--- webpage/tailwind.config.js
+++ webpage/tailwind.config.js
@@ -7,12 +7,12 @@ module.exports = {
     extend: {
       keyframes: {
         fadeIn: {
-          '0%': { opacity: '0.5', transform: 'translateY(5px)' },
+          '0%': { opacity: '0', transform: 'translateY(5px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
         }
       },
       animation: {
-        fadeIn: 'fadeIn 0.3s ease-out',
+        fadeIn: 'fadeIn 0.5s ease-out',
       }
     },
   },
