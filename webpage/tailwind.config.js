9f33fb243e92b7e79311e7d56aa82487f8222979
Nigatu Paulos <nigatujeno@gmail.com>
2025-12-02 23:24:59 -0500

    add fade in animation for logs (webpage)

--- webpage/tailwind.config.js
+++ webpage/tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0.5', transform: 'translateY(5px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.3s ease-out',
+      }
+    },
   },
   plugins: [],
 }
